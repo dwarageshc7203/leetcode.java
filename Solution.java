e70a22478d2165bfd3fbbcaf501e139bb790733b
dwarageshc7203 <dwarageshc7203@gmail.com>
2025-12-17 20:46:25 +0530

    trial

--- Solution.java
+++ Solution.java
@@ -1,19 +1,27 @@
 class Solution {
-    public int[] twoSum(int[] nums, int target) {
-        HashMap<Integer, Integer> hash = new HashMap();
-        int a = 0;
-        int b = 0;
-
-        for( int i = 0; i< nums.length; i++){
-            int complement =target - nums[i];
-            if(hash.containsKey(complement)){
-                a = hash.get(complement);
-                b = i;
-            }
-            else{
-                hash.put( nums[i] , i );
+    public int maxProfit(int[] prices) {
+        int min=Integer.MAX_VALUE;
+        int minPtr= 0;
+        int max=0;
+        for( int i = 0; i< prices.length - 1; i++){
+            if(prices[i] < min && prices[i+1] > prices[i]){
+                min = prices[i];
+                minPtr = i;
             }
         }
-        return new int[] {a,b};
+        for( int i = minPtr; i< prices.length; i++){
+            if(prices[i] > min && prices[i] > max){
+                max = prices[i];
+            }
+        }
+        System.out.println(min);
+        System.out.println(max);
+        int profit = max - min;
+        return profit;
+    }
+    public static void main(String[] args){
+        Solution obj= new Solution();
+        int[] prices = {7,1,5,3,6,4};
+        System.out.println(obj.maxProfit(prices));
     }
 }
