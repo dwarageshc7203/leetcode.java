8254d0be24bba07a0bca78840bb31930b0308c41
dwarageshc7203 <dwarageshc7203@gmail.com>
2025-12-18 23:40:59 +0530

    terenary operator

--- jFullStack.java
+++ jFullStack.java
@@ -3,9 +3,8 @@ public class jFullStack {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int a = scanner.nextInt();
-        if( a > 0)
-            System.out.println("Hello");
-        else
-            System.out.println("bye");
+        
+        boolean result = a%2 == 0 ? true : false;
+        System.out.println(result);
     }
 }
