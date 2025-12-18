55461eb6eb52088905ba022080a1f2c51cfa27db
dwarageshc7203 <dwarageshc7203@gmail.com>
2025-12-18 23:39:01 +0530

    Changes done

--- jFullStack.java
+++ jFullStack.java
@@ -3,14 +3,9 @@ public class jFullStack {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int a = scanner.nextInt();
-        if( a > 0){
+        if( a > 0)
             System.out.println("Hello");
-        }
-        else if( a == 0){
-            System.out.println("Zero");
-        }
-        else{
-            System.out.println("Hero");
-        }
+        else
+            System.out.println("bye");
     }
 }
