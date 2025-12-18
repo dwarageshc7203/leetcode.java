77b7bec62f9edfd5c2c7df9d7c2c11479c4a1522
dwarageshc7203 <dwarageshc7203@gmail.com>
2025-12-18 23:32:40 +0530

    Trial

--- jFullStack.java
+++ jFullStack.java
@@ -1,3 +1,16 @@
+import java.util.Scanner;
 public class jFullStack {
-    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        if( a > 0){
+            System.out.println("Hello");
+        }
+        else if( a == 0){
+            System.out.println("Zero");
+        }
+        else{
+            System.out.println("Hero");
+        }
+    }
 }
