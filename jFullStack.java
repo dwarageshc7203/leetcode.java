60729bb396de8080c51574db4c75e9a6c841a038
dwarageshc7203 <dwarageshc7203@gmail.com>
2025-12-19 11:33:54 +0530

    format specifiers

--- jFullStack.java
+++ jFullStack.java
@@ -1,10 +1,10 @@
-import java.util.Scanner;
 public class jFullStack {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int a = scanner.nextInt();
-        
-        boolean result = a%2 == 0 ? true : false;
-        System.out.println(result);
+        for( int i = 1; i<= 5; i++){
+            System.out.println("Day "+i);
+            for ( int j= 9; j<= 20; j++){
+                System.out.println(String.format("Time: %d hours", j)   );
+            }
+        }
     }
 }
