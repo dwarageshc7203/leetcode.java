7fadfbf12c6074dfd674f1bdfd3d3b638d9410d2
dwarageshc7203 <dwarageshc7203@gmail.com>
2025-12-22 22:28:36 +0530

    works

--- Solution.java
+++ Solution.java
@@ -1,60 +1,71 @@
-//Given an m x n grid of characters board and a string word, return true if word exists in the grid.
+import java.util.ArrayList;
+import java.util.List;
 
 class Solution {
-    public boolean exist(char[][] board, String word) {
-
+    public List<String> findWords(char[][] board, String[] words) {
+        //init the row and column
         int row = board.length;
         int column = board[0].length;
+        int len = words.length;
+        List<String> output = new ArrayList<>();
 
-        for( int i = 0; i < row; i++){
-            for( int j = 0; j < column; j++){
-                if(movement(board, i, j, 0, word))
-                    return true;
+        for (int k = 0; k < len; k++) {
+            boolean found = false;
+            for (int i = 0; i < row && !found; i++) {
+                for (int j = 0; j < column && !found; j++) {
+                    if(movement(board, words, i , j, k , 0)){
+                        output.add(words[k]);
+                        found = true;
+                    }
+                }
             }
         }
-
-        return false;
+        return output;
     }
 
-    public boolean movement(char[][] board, int row, int column, int index, String word){
-
-        if(index == word.length()){
+    //movement
+    public boolean movement(char[][] board, String[] words, int row, int column, int k, int index){
+        
+        if(index == words[k].length()){
             return true;
         }
 
-        if( row < 0 ||
+        if( 
+            row < 0 ||
             column < 0 ||
             row >= board.length ||
             column >= board[0].length ||
-            board[row][column] != word.charAt(index)
-        ){
+            board[row][column] != words[k].charAt(index)
+            ){
             return false;
         }
 
         char temp = board[row][column];
         board[row][column] = '$';
 
-        //I implemented backtracking here hehe
-
-        boolean result = (
-            movement(board, row + 1, column, index+1, word) ||
-            movement(board, row, column + 1, index+1, word) ||
-            movement(board, row - 1, column, index+1, word) ||
-            movement(board, row, column - 1, index+1, word)
+        boolean result =(
+             movement( board, words, row + 1, column, k, index + 1) || 
+             movement( board, words, row - 1, column, k, index + 1) ||
+             movement( board, words, row, column + 1, k, index + 1) ||
+             movement( board, words, row, column - 1, k, index + 1)
         );
-
         board[row][column] = temp;
 
         return result;
-
     }
 
-    //main method
-    public static void main( String[] args){
-        char[][] board = {{'A', 'B', 'C', 'E'}, {'S', 'F', 'C', 'S'} }; //[["A","B","C","E"],["S","F","C","S"]
-        String word = "ABCCED";
-        Solution obj = new Solution();
-        System.out.println(obj.exist(board, word));
+    public static void main(String[] args){
+        char[][] board = {
+    {'o','a','a','n'},
+    {'e','t','a','e'},
+    {'i','h','k','r'},
+    {'i','f','l','v'}
+};
+
+String[] words = {"oath", "pea", "eat", "rain"};
+
+Solution obj = new Solution();
+
+System.out.println(obj.findWords(board, words));
     }
 }
-
