ff69f3658847a6ee4802a109570cbd02abd8a306
dwarageshc7203 <dwarageshc7203@gmail.com>
2025-12-31 12:00:24 +0530

    Exception Handling

--- Exercise.java
+++ Exercise.java
@@ -1,17 +1,20 @@
-
-@FunctionalInterface
-interface A{
-    public String name(String name);
-}
-
 public class Exercise{
     public static void main(String[] args){
-        A obj = (name) -> {
-            return ("Your name is " + name);
-        };
-        String answer = obj.name("CNL");
+        int i = 0;
+        int j = 0;
+        System.out.println("Initial i: " + i);
+        System.out.println("Initial j: " + j);
 
-        System.out.println(answer);
-        System.out.println(obj.name("CNL"));
+        try
+        {
+            j = 18 / i;
+        }
+        catch(Exception e)
+        {
+            System.out.println("Problem with the value of i ( " + i + " )");
+        }
+
+        System.out.println("Final value of i: " + i);
+        System.out.println("Final value of j: " + j);
     }
 } 
