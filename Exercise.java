b4f6d2ffb73aa93e21957d994ab15070c8845b13
dwarageshc7203 <dwarageshc7203@gmail.com>
2026-01-03 13:58:26 +0530

    Class sorted

--- Exercise.java
+++ Exercise.java
@@ -1,22 +1,44 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
-public class Exercise {
-    
-    public static void main(String[] args){
-        
-        List<String> names = Arrays.asList("Alice", "Bob", "Alice", "David", "Bob");
-        
-        // TODO: Print the original list
+class Student{
+    int age;
+    String name;
 
-        System.out.println("Original List: " + names);
-        
-        Set<String> renames = new LinkedHashSet<String>(names);
-
-        // TODO: Convert the list to a LinkedHashSet to remove duplicates while preserving order
-
-        // TODO: Print the set of unique names
-        System.out.println("Unique Names:  " + renames);
-        
+    //constructor
+    public Student(int age, String name) {
+        this.age = age;
+        this.name = name;
     }
 
+    //toString
+    @Override
+    public String toString() {
+        return "Student [age=" + age + ", name=" + name + "]";
+    }
+}
+
+public class Exercise{
+    public static void main(String[] args) {
+        List <Student> nums = new ArrayList<>();
+        
+        nums.add(new Student(19, "CNL"));
+        nums.add(new Student(20, "BNL"));
+        nums.add(new Student(18, "DNL"));
+
+        Comparator<Student> com = new Comparator<Student>() {
+            public int compare(Student i, Student j){
+                if(i.age > j.age){
+                return 1;
+                }
+                else{
+                    return -1;
+                }
+            }
+        };
+        Collections.sort(nums, com);
+        System.out.println(nums);
+    }
 }
