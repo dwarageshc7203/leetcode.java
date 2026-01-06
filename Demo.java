5808142c73ec3a362cb123cddca2fda9702ba26e
dwarageshc7203 <dwarageshc7203@gmail.com>
2026-01-06 22:41:15 +0530

    Leetcode 4x problems

--- Demo.java
+++ Demo.java
@@ -1,32 +1,30 @@
 class Solution {
-    public long maxMatrixSum(int[][] matrix) {
+    public int maxProfit(int[] prices) {
 
-        int negC = 0;
-        long sum = 0;
-        long minabs = Integer.MAX_VALUE;
-
-        for( int i = 0; i < matrix.length; i++){
-            for( int j = 0; j < matrix[0].length; j++){
-
-                int value = matrix[i][j];
-
-                if(value < 0){
-                    negC++;
-                }
-                minabs = Math.min( minabs, Math.abs(value));
-                sum += Math.abs((long)value);
+        int max = 0;
+        int min = Integer.MAX_VALUE;
+        for( int p : prices)
+            if(p < min)
+            {
+                min = p;
             }
-        }
-        if(negC % 2 == 1)
-            sum -= 2L * minabs;
 
-        return sum;
+            else if(max < p - min)
+            {
+                max = p - min;
+            }
+        return max;
     }
 }
+
 public class Demo{
     public static void main(String[] args) {
+        
+
         Solution obj = new Solution();
-        int[][] matrix = {{1, -1}, {-1, 1} };
-        System.out.println(obj.maxMatrixSum(matrix));        
+
+        int[] prices = {7,1,5,3,6,4};
+
+        System.out.println(obj.maxProfit(prices));
     }
 }
