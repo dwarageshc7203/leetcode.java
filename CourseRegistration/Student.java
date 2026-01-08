5a2308955d7bee6cdfe0d04405a7b5dd7e0326d7
dwarageshc7203 <dwarageshc7203@gmail.com>
2026-01-08 23:18:54 +0530

    .

--- CourseRegistration/Student.java
+++ CourseRegistration/Student.java
@@ -3,13 +3,30 @@ package CourseRegistration;
 import java.sql.*;
 
 public class Student {
-    private int age;
-    private String name;
+    private int age = 0;
+    private String name = "";
 
-    Connection con = 
+    //constructor
+    public Student(int age, String name){
+        this.age = age;
+        this.name = name;
 
-    public void addStudent()
-    {
+        System.out.println("Recorded. Name: " + this.name + " ; Age: " + this.age);
+    }
 
+    //create Student
+    public void createStud(int age, String name) throws SQLException{
+        
+        Connection con = DriverManager.getConnection(
+            "jbdc:postgres://localhost:5432/demo",
+            "postgres",
+            "dwarageshdc"
+        );
+
+         System.out.println("Connection Established");
+        
+        PreparedStatement insert = con.prepareStatement("insert into student values (?, ?)");
+        insert.setInt(1, this.age);
+        insert.setString(2, this.name);
     }
 }
