b1c9ad1b2ea9fee3da5ac4f57ce433ec1a5839c7
dwarageshc7203 <dwarageshc7203@gmail.com>
2026-01-09 22:46:03 +0530

    .

--- Solution.java
+++ Solution.java
@@ -1,35 +1,77 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
 class Solution {
-    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        int carry = 0;
-        ListNode dummy = new ListNode(0);
-        ListNode current = dummy;
 
-        while(l1 != null || l2 != null || carry != 0)
-        {
-            int digit1 = (l1 != null) ? l1.val : 0;
-            int digit2 = (l2 != null) ? l2.val : 0;
-            int sum = digit1 + digit2 + carry;
-            carry = sum / 10;
+    private static final int GRID_SIZE = 9;
 
-            current.next = new ListNode(sum % 10);            
-            current = current.next;
-            if(l1 != null){
-                l1 = l1.next;
-            }
-            if(l2 != null){
-                l2 = l2.next;
+    private static boolean inRow(char[][] board, char number, int row) {
+        for (int i = 0; i < GRID_SIZE; i++) {
+            if (board[row][i] == number) {
+                return true;
             }
         }
-        return dummy.next;
+        return false;
     }
+
+    //for the column
+    private static boolean inColumn(char[][] board, int column, char number) {
+        for (int i = 0; i < GRID_SIZE; i++) {
+            if (board[i][column] == number) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    //for the box
+    private static boolean inBox(char[][] board, int row, int column, char number) {
+        int localRow = row - row % 3;
+        int localColumn = column - column % 3;
+
+        for (int i = localRow; i < localRow + 3; i++) {
+            for (int j = localColumn; j < localColumn + 3; j++) {
+                if (board[i][j] == number) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    //check valididty
+    private static boolean isValid(char[][] board, int row, int column, char number) {
+        return !inRow(board, number, row) &&
+                !inColumn(board, column, number) &&
+                !inBox(board, row, column, number);
+    }
+
+    //actual solver logic
+    private static boolean sudokuSolver(char[][] board) {
+        for (int row = 0; row < GRID_SIZE; row++) {
+            for (int column = 0; column < GRID_SIZE; column++) {
+                if (board[row][column] == '.') {
+                    for (int i = 1; i <= GRID_SIZE; i++) {
+                        if (isValid(board, row, column, (char)(i + '0'))) {
+                            board[row][column] = (char)(i + '0');
+
+                            if (sudokuSolver(board)) {
+                                return true;
+                            } else {
+                                board[row][column] = '.';
+                            }
+                        }
+                    }
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+
+    public void solveSudoku(char[][] board) {
+        sudokuSolver(board);
+    //main function here hehe
+
+    //for the row
+
+}
 }
