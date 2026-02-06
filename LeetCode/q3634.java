3123ce2f8708a17a64bc046ff7eb0c255d5844e2
dwarageshc7203 <dwarageshc7203@gmail.com>
2026-02-06 21:57:32 +0530

    .

---
+++ LeetCode/q3634.java
@@ -0,0 +1,16 @@
+// class Solution {
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+class q3634 {
+    public int minRemoval(int[] nums, int k) {
+        Arrays.sort(nums);
+        
+        for ( int i = 0; i < nums.length; i ++){
+            
+        }
+        return 0;
+    }
+}
